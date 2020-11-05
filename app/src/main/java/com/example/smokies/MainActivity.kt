7c42ef893e6d9ad67782f033cd60f8e7292210f5
kenei <kevinkenei9@gmail.com>
2020-11-05 13:18:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/smokies/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.smokies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Smokies)
+        setContentView(R.layout.activity_main)
+
+    }
+}
